3ce0e5f986667ffbd2fcadf8db30e96e5801be0f
naomilk <88796940+naomilk@users.noreply.github.com>
2021-08-18 11:30:54 +0100

    Add files via upload
    This JS file calculates your age in dog years if you change your age in the MyAge variable. It doesn't have a user input feature yet.

---
+++ MyAgeInDogYears.js
@@ -0,0 +1,9 @@
+const MyAge = 20;
+let EarlyYears = 2;
+EarlyYears *= 10.5;
+let LaterYears = MyAge - 2;
+LaterYears *= 4;
+const MyAgeInDogYears = EarlyYears + LaterYears;
+const MyName = 'Naomi Kangwa' .toLowerCase();
+
+console.log(`My name is ${MyName}. I am ${MyAge} years old in human years which is ${MyAgeInDogYears} years old in dog years.`);
