78a9388f5054686ad30d03570712c8be85ba6040
naomilk <88796940+naomilk@users.noreply.github.com>
2021-08-19 10:18:43 +0100

    Add files via upload

---
+++ magiceightball.js
@@ -0,0 +1,41 @@
+let userName ='Naomi';
+userName ? console.log(`Hello, ${userName}!`) : ('Hello!');
+
+let userQuestion = 'Will I get married?';
+console.log(`${userName} asked: ${userQuestion}.`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch(randomNumber) {
+  case 0:
+    eightBall = 'Nope...';
+    break;
+  case 1:
+    eightBall = 'It is certain';
+    break;
+  case 2:
+    eightBall = 'It is decidedly so';
+    break;
+  case 3:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 4:
+    eightBall = 'Cannot predict now';
+    break;
+  case 5:
+    eightBall = ('Do not count on it');
+    break;
+  case 6:
+    eightBall = ('My sources say no');
+    break;
+  case 7:
+    eightBall = ('Outlook not so good');
+    break;
+  case 8:
+    eightBall = ('Signs point to yes');
+    break;
+}
+
+console.log(`The eight ball answered: ${eightBall}`); 
