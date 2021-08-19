f6e91c8435b188e2c1ec4f7e5e73d72fd3e5f872
naomilk <88796940+naomilk@users.noreply.github.com>
2021-08-19 11:04:30 +0100

    Add files via upload
    This took a while to do... nested 'if' statements baffled me a little at first, but got there in the end!

---
+++ RockPaperScissors.js
@@ -0,0 +1,60 @@
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase()
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
+      return userInput
+    } else {
+      console.log ('Error!')
+    }
+  }
+  
+  const getComputerChoice = () => {
+    randomNumber = Math.floor(Math.random() * 3);
+    switch(randomNumber) {
+      case 0:
+        return 'rock';
+      case 1:
+        return 'paper';
+      case 2:
+        return 'scissors';
+    }
+  }
+  
+  function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+      return 'The game was a tie!';
+    }
+    if (userChoice === 'bomb') {
+      return 'You won unconditionally!!!'
+    }
+    if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+    if (userChoice === 'paper') {
+      if (computerChoice === 'scissors') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+    if (userChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+    }
+  
+  const playGame = () => {
+    const userChoice = getUserChoice('paper');
+    const computerChoice = getComputerChoice();
+    console.log('You threw ' + userChoice);
+    console.log('The computer threw ' + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+  }
+    
+  playGame();
